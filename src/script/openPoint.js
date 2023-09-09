6269ee92eef80eda91918baa10acef935c3909a5
yusivacova <sivacovayulia@gmail.com>
2023-09-09 21:50:56 +0300

    feat: add functionality of changing point icon

--- src/script/openPoint.js
+++ src/script/openPoint.js
@@ -1,6 +1,17 @@
-import { ACTIVE_CLASS_BUTTON, INITIAL_CLASS_BUTTON, INNER_BUTTON_CLASS } from './dataButtons';
+import {
+  ACTIVE_CLASS_BUTTON, BUTTON_ICON_CONTENT, INITIAL_CLASS_BUTTON, INNER_BUTTON_CLASS,
+} from './dataButtons';
 
 /* eslint-disable import/prefer-default-export */
+function changeIconButton(element) {
+  const iconElement = element;
+  if (iconElement.textContent === BUTTON_ICON_CONTENT.close) {
+    iconElement.textContent = BUTTON_ICON_CONTENT.open;
+  } else {
+    iconElement.textContent = BUTTON_ICON_CONTENT.close;
+  }
+}
+
 function togglePointButton(element) {
   element.classList.toggle(ACTIVE_CLASS_BUTTON.open);
 }
@@ -10,9 +21,13 @@ function removeTitlePointButton(element) {
 }
 
 function removeTitlePointButtonActive() {
-  const openPoint = document.querySelector(`.${ACTIVE_CLASS_BUTTON.open}`);
-  if (openPoint) {
-    removeTitlePointButton(openPoint);
+  const openPointButtons = document.querySelectorAll(`.${ACTIVE_CLASS_BUTTON.open}`);
+  if (openPointButtons.length) {
+    openPointButtons.forEach((point) => {
+      const iconElement = point.previousElementSibling;
+      removeTitlePointButton(point);
+      changeIconButton(iconElement);
+    });
   }
 }
 
@@ -23,17 +38,22 @@ export function buttonPointHandler() {
     const clickedTag = e.target;
 
     if (clickedTag.className.includes(INNER_BUTTON_CLASS.title)) {
+      const iconElement = clickedTag.previousElementSibling;
       removeTitlePointButton(clickedTag);
+      changeIconButton(iconElement);
     }
 
     if (clickedTag.className === INNER_BUTTON_CLASS.icon) {
       const titleElement = clickedTag.nextElementSibling;
       togglePointButton(titleElement);
+      changeIconButton(clickedTag);
     }
 
     if (clickedTag.className.includes(INITIAL_CLASS_BUTTON)) {
+      const iconElement = clickedTag.firstElementChild;
       const titleElement = clickedTag.lastElementChild;
       togglePointButton(titleElement);
+      changeIconButton(iconElement);
     }
 
     if (!clickedTag.className.includes('point')) {
